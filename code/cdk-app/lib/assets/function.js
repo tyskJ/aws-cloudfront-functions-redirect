fb41dfe5cef3d2bc873e2ce1fb04a1437f4dcc41
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-09 21:49:10 +0900

    add function

---
+++ code/cdk-app/lib/assets/function.js
@@ -0,0 +1,26 @@
+function handler(event) {
+  var request = event.request;
+  var uri = request.uri;
+  var newurl = `https://${FqdnForAlb}/`;
+
+  // Check uri
+  if (uri === "/test1/") {
+    newurl = `https://${FqdnForAlb}/test1/`;
+  }
+  // Check uri
+  else if (uri === "/test2/") {
+    newurl = `https://${FqdnForAlb}/test2/`;
+  } else {
+    return request;
+  }
+  var response = {
+    statusCode: 301,
+    statusDescription: "Moved Permanently",
+    headers: {
+      location: {
+        value: newurl,
+      },
+    },
+  };
+  return response;
+}
