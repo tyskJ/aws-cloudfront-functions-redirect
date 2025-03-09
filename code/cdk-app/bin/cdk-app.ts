b51a2337a27ab08b5944ff51a5c937432c89c503
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-09 21:58:46 +0900

    add TokyoStack

--- code/cdk-app/bin/cdk-app.ts
+++ code/cdk-app/bin/cdk-app.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { VirginiaStack } from "../lib/stack/VirginiaStack";
+import { TokyoStack } from "../lib/stack/TokyoStack";
+import { devParameter } from "../parameter";
 
 const app = new cdk.App();
 
@@ -13,3 +15,12 @@ const virginia = new VirginiaStack(app, "VirginiaStack", {
   description: "Virginia Region Stack.",
 });
 cdk.Tags.of(virginia).add("Env", "Virginia");
+
+// Tokyo Stack
+const tokyo = new TokyoStack(app, "TokyoStack", {
+  crossRegionReferences: true,
+  ...devParameter,
+  cfCert: virginia.CfCertificate,
+  description: "Tokyo Region Stack.",
+});
+cdk.Tags.of(tokyo).add("Env", devParameter.EnvName);
--- code/cdk-app/lib/stack/TokyoStack.ts
+++ code/cdk-app/lib/stack/TokyoStack.ts
@@ -1,8 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { Parameter } from "../../parameter";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+
+export interface TokyoStackProps extends Parameter {
+  cfCert: acm.Certificate;
+}
 
 export class TokyoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: TokyoStackProps) {
     super(scope, id, props);
   }
 }
